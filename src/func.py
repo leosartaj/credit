1e7fb4d2457461f7576fc29a6fa7da19c7a2715a
Sartaj Singh <singhsartaj94@gmail.com>
2014-09-16 23:25:04 +0530

    improved functions

--- src/func.py
+++ src/func.py
@@ -18,44 +18,46 @@ def pDir():
     """
     return os.getcwd()
 
-def update(fName, num, method='add'):
+def add(fName, num, dire=pDir()):
     """
-    update a file
+    add a num to file
     """
     f = open(fName, 'a')
-    if method == 'sub':
-        f.write(' - ' + str(num))
-    else:
-        f.write(' + ' + str(num))
+    f.write(' + ' + str(num))
     f.close()
 
-#update('new', '10')
-#update('new', '10', 'sub')
-#update('new', '10')
+def sub(fName, num, dire=pDir()):
+    """
+    subtract a num to file
+    """
+    f = open(fName, 'a')
+    f.write(' - ' + str(num))
+    f.close()
 
-def display():
+#add('new', '10')
+#sub('new', '10')
+#add('new', '10')
+
+def display(dire=pDir()):
     """
     gives the list of files
     """
-    l = []
-    for item in os.walk(pDir()):
-        l.append(item)
     accs = ''
-    for acc in l[0][2]:
-        accs += acc + ' '
+    for filename in os.listdir(dire):
+        accs += filename + ' '
     return 'Accounts -> ' + accs
 
 #print display()
 
-def total(fName):
+def total(fName, dire=pDir()):
     """
     finds the total balance with a particular file
     """
-    f = open(fName, 'r')
+    f = open(os.path.join(dire, fName), 'r')
+    net = 0
     for line in f:
         line = line.replace('\n', '')
         line = line.split(' ')
-        net = 0
         for i in range(1, len(line)):
             if line[i] == '+' or line[i] == '-':
                 continue
@@ -68,24 +70,34 @@ def total(fName):
 
 #print total('new')
 
-def display_acc(fName):
+def display_acc(fName, dire=pDir()):
     """
     displays the content of a file
     """
-    f = open(fName, 'r')
+    f = open(os.path.join(dire, fName), 'r')
     acc = fName + ' ->'
     for line in f:
         acc += line
     f.close()
     return acc
 
+print display_acc('new')
+
+def reset(fName, dire=pDir()):
+    """
+    Reset an account to 0
+    """
+    f = open(os.path.join(dire, fName), 'w')
+    f.close()
+
+#reset('new')
 #print display_acc('new')
 
-def rem_last(fName):
+def rem_last(fName, dire=pDir()):
     """
     remove the last entry from file
     """
-    f = open(fName, 'r')
+    f = open(os.path.join(dire, fName), 'r')
     rline = []
     for line in f:
         rline = line.replace('\n', '')
